2b811a6cd63d217d35454b852067597417741703
Walter <walterlaicollege@gmail.com>
2023-02-12 16:18:25 -0500

    added orthogonal iteration function

--- functions.py
+++ functions.py
@@ -14,15 +14,52 @@ def gram_schmidt(M):
 
     return Q,R
 
-np.random.seed(123)
-size = 1000
-M = np.random.random((size,size))
-start = time.time()
-Q, R = gram_schmidt(M)
-print(time.time()-start)
-I_expected   = np.identity(size)
-I_observed   = np.dot(Q.T, Q)  #Are the columns of Q all orthogonal to each other?  Are they all length of 1?
-I_difference = np.sqrt(np.sum((I_expected-I_observed)**2))
-A_difference = np.sqrt(np.sum((M-(Q@R))**2)) #Does the produce of Q and R equal the original input matrix?
+def test_gram_schmidt():
+    np.random.seed(123)
+    size = 100
+    M = np.random.random((size,size))
+    start = time.time()
+    Q, R = gram_schmidt(M)
+    print(time.time()-start)
+    I_expected   = np.identity(size)
+    I_observed   = np.dot(Q.T, Q)  #Are the columns of Q all orthogonal to each other?  Are they all length of 1?
+    I_difference = np.sqrt(np.sum((I_expected-I_observed)**2))
+    A_difference = np.sqrt(np.sum((M-(Q@R))**2)) #Does the produce of Q and R equal the original input matrix?
+    return I_difference, A_difference
+
+def orthogonal_iteration(M, nsteps=20):
+    A = M.astype(float)
+    m,n = A.shape
+    Q = np.identity(m) #eigenvectors
+    for step in range(nsteps):
+        AQ = np.dot(A, Q)
+        Q,R= gram_schmidt(AQ)
+    '''
+    Why do we just return the R as the eigenvalues?
+    At convergence, AQ has every column orthogonal but not all length 1.
+    Subsequent A x Q will just be scaling the columns of Q.
+    If AQ = QxR, R is a matrix that scales the columns of Q.
+    Therefore, R would approach a diagonal matrix containing the eigenvalues.
+    '''
+    return np.diag(R), Q
+
+def test_orthogonal_iteration():
+    A = np.array([
+        [1,3,2,4],
+        [4,5,6,2],
+        [7,8,9,5],
+        [5,2,7,8]    
+    ])
+    Q, R = gram_schmidt(A)
+    eigvalues  = np.array([
+        [4,0,0,0],
+        [0,3,0,0],
+        [0,0,2,0],
+        [0,0,0,1]
+    ])
+    M = Q @ eigvalues @ Q.T
+    print("The expected eigvals :", np.linalg.eigvals(M))
+    S,X = orthogonal_iteration(M)
+    print("The observed eigenvas:",  S)
 
 print()
